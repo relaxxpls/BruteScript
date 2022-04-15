ead1de8f1adf3ffb79a92b6fe663da6a4d5d2245
Laxman Desai <lolopaw24@gmail.com>
2022-04-15 05:38:59 +0530

    👌 IMPROVE: Update syntax

--- PowerShell C++ Script/correct.cpp
+++ PowerShell C++ Script/correct.cpp
@@ -1,20 +1,22 @@
 // -------------------------- Author: relaxxpls -------------------------- //
 #include <bits/stdc++.h>
 using namespace std;
-#define int             long long
+#define int long long
 
 void solve() {
-    int n; cin>>n;
-    int a[n];
-    for(int i=0; i<n; i++)
-        cin>>a[i];
-    sort(a, a+n);	// correct way of sorting
-    cout<<a[1]<<endl;
+  int n;
+  cin >> n;
+  int a[n];
+  for (int i = 0; i < n; i++)
+    cin >> a[i];
+  sort(a, a + n); // correct way of sorting
+  cout << a[1] << endl;
 }
 
 int32_t main() {
-	int t = 1; cin>>t;	// no of test cases, default 1
-    while(t--)
-    	solve();
-    return 0;
+  int t = 1;
+  cin >> t; // no of test cases, default 1
+  while (t--)
+    solve();
+  return 0;
 }
--- PowerShell C++ Script/inputgen.cpp
+++ PowerShell C++ Script/inputgen.cpp
@@ -4,21 +4,23 @@ using namespace std;
 #define int long long
 #define chr std::chrono
 
-int rand(int a, int b) {
-	return a + rand()%(b-a+1);
-}
+int rand(int a, int b) { return a + rand() % (b - a + 1); }
 
 int32_t main() {
-	int seed = chr::duration_cast<chr::milliseconds>(chr::system_clock::now().time_since_epoch()).count();
-	srand(seed);
+  int seed = chr::duration_cast<chr::milliseconds>(
+                 chr::system_clock::now().time_since_epoch())
+                 .count();
+  srand(seed);
 
-	cout<<1<<endl;	// keep num of test cases 1
-	int n = rand(1, 100), m = rand(1, 100);
-	cout<<n<<" "<<m<<endl;
-    for(int i=0; i<n; i++) {
-    	for(int j=0; j<m; j++)
-    		cout<<rand(0, 1e9)<<" ";
-    	cout<<endl;
-    }
-    return 0;
+  cout << 1 << endl; // keep num of test cases 1
+  int n = rand(1, 100), m = rand(1, 100);
+  cout << n << " " << m << endl;
+
+  for (int i = 0; i < n; i++) {
+    for (int j = 0; j < m; j++)
+      cout << rand(0, 1e9) << " ";
+    cout << endl;
+  }
+
+  return 0;
 }
